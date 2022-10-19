65bdc2a7fba2486144eb81163c3de890f71e8fb7
Devin Jaggernauth <neexis@gmail.com>
2022-10-19 07:14:57 -0400

    add link to logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Header.css';
 import headerLogo from '../../images/logo.svg';
 import Navigation from '../Navigation/Navigation';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={headerLogo} alt="" className="header__logo" />
+      <Link to="/">
+        <img src={headerLogo} alt="" className="header__logo" />
+      </Link>
       <Navigation />
     </header>
   );
