0012b0df1b06ceaff1317215bd232d29e3a11e54
Devin Jaggernauth <neexis@gmail.com>
2022-10-19 21:57:38 -0400

    create main

---
+++ frontend/src/components/Main/Main.tsx
@@ -0,0 +1,21 @@
+import './Main.css'
+
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+    <div className="adopt">
+      <div className="adopt__text">
+
+      <h2 className="adopt__title">Adopt pets in your area</h2>
+      <p className="adopt__text">Find cats, dogs and other pets available right near you.</p>
+      <button className="adopt__button">Adopt</button>
+      </div>
+      <img src="" alt="" className="adopt__image" />
+    </div>
+    </>
+  )
+}
+
+export default Main
