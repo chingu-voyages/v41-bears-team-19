de38876f481b8846fc38cd48708eba9b163c70a2
Amanda Vieira <amanda.alexandre@outlook.com>
2022-10-20 01:29:08 -0300

    style: added general styles

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -3,7 +3,12 @@ import './Hero.css'
 
 const Hero = () => {
     return (
-        <div className='hero'>Hero section</div>
+        <div className='hero'>
+            <h1>Find your next friend</h1>
+            <h2>Adopt pets in your area or help animal shelters.</h2>
+
+            <button>Start here</button>
+        </div>
     )
 }
 
