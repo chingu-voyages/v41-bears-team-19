1d2cc5aeac33291cf9e811fe1b2b900855664540
Devin Jaggernauth <neexis@gmail.com>
2022-10-25 10:08:47 -0400

    remove extra components from App.tsx

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -15,7 +15,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Hero />} />
       </Routes>
-      <Hero />
       <Team />
       <Footer />
     </div>
