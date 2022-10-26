1987e0aca2bcaa775b1e4eb39d84f585a80171b8
Devin Jaggernauth <neexis@gmail.com>
2022-10-26 18:05:01 -0400

    create component

---
+++ frontend/src/components/Register/Register.tsx
@@ -0,0 +1,20 @@
+import './Register.css'
+import React from 'react'
+import FormPage from '../FormPage/FormPage'
+import registerDecoration from '../../images/spiral-down-pink.png'
+
+const Register = () => {
+
+  const handleSubmit = () =>{
+
+  }
+
+  return (
+    <FormPage title='Register' decoration={registerDecoration} submitHandler={handleSubmit}>
+      <>
+      </>
+    </FormPage>
+  )
+}
+
+export default Register
