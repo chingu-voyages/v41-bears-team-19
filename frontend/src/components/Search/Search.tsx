ba6444228976e8829258c7d49ee963e0c195f917
Devin Jaggernauth <neexis@gmail.com>
2022-10-26 22:06:14 -0400

    set buttons to links

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -3,6 +3,7 @@ import searchDecoration from '../../images/spiral-down-pink.png';
 import searchPet from '../../images/search-pet.png';
 import React from 'react';
 import FormPage from '../FormPage/FormPage';
+import { Link } from 'react-router-dom';
 
 const Search = () => {
   const handleSubmit = () => {
@@ -24,13 +25,14 @@ const Search = () => {
           placeholder="Type your city"
           required
         />
-        <input
-          type="submit"
-          className="form__submit form__submit_search"
-        />
+        <input type="submit" className="form__submit form__submit_search" />
         <div className="search__buttons">
-          <button className="search__button">Login</button>
-          <button className="search__button">Register</button>
+          <Link to="/login" className="search__link">
+            <button className="search__button">Login</button>
+          </Link>
+          <Link to="/register" className="search__link">
+            <button className="search__button">Register</button>
+          </Link>
         </div>
         <img src={searchPet} alt="" className="search__image" />
       </>
