4736e95507d99c6a95111f18de9758b6469da8d1
Devin Jaggernauth <neexis@gmail.com>
2022-10-31 21:16:11 -0400

    create adopt page

---
+++ frontend/src/components/AdoptPage/AdoptPage.tsx
@@ -0,0 +1,13 @@
+import './AdoptPage.css'
+
+import React from 'react'
+
+const AdoptPage = () => {
+  return (
+    <section className="adopt">
+      
+    </section>
+  )
+}
+
+export default AdoptPage
