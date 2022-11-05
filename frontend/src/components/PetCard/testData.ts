c229f7cd06cb9f7b2b8c84e0cef6293ba66e3557
Devin Jaggernauth <neexis@gmail.com>
2022-11-05 07:33:11 -0400

    add test data

---
+++ frontend/src/components/PetCard/testData.ts
@@ -0,0 +1,24 @@
+export const pets = [
+  {
+    name: 'Barnaby',
+    age: 1,
+    location: 'Boulder, Colorado',
+    type: 'Bunny',
+    photo: '../../images/barnaby.png',
+  },
+  {
+    name: 'Flora',
+    age: 1,
+    location: 'Boulder, Colorado',
+    type: 'Dog',
+    photo: '../../images/flora.png',
+  },
+  {
+    name: 'Amy',
+    age: 1,
+    location: 'Boulder, Colorado',
+    type: 'Cat',
+    photo: '../../images/amy.png',
+  },
+
+];
