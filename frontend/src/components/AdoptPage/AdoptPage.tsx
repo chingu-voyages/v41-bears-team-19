0e9ffefd4e158e52dea2990183cb98af9386a909
Devin Jaggernauth <neexis@gmail.com>
2022-11-05 08:35:42 -0400

    add categories

--- frontend/src/components/AdoptPage/AdoptPage.tsx
+++ frontend/src/components/AdoptPage/AdoptPage.tsx
@@ -2,14 +2,15 @@ import PetCard from '../PetCard/PetCard';
 import './AdoptPage.css';
 import { pets } from './testData';
 import { Pet } from '../PetCard/PetCard';
+import Categories from '../Categories/Categories';
 
 const AdoptPage = () => {
   const results = pets.map((pet: Pet) => <PetCard pet={pet} key={pet.id}/>);
 
   return (
     <section className="adopt">
-      <h1 className="adopt___title">Adopt Your Pet</h1>
-      {/* <Categories /> */}
+      <h1 className="adopt__title">Adopt Your Pet</h1>
+      <Categories />
       <section className="results">{results}</section>
     </section>
   );
