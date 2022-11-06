efe765071abda10e60d79aa74875432b6e05528b
Devin Jaggernauth <neexis@gmail.com>
2022-11-06 10:25:41 -0400

    create context

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { context } from './context';
 
 import { schema } from './schema';
 export const server = new ApolloServer({
   schema,
+  context,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
 });
 
