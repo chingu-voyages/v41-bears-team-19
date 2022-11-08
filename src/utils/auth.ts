3bfa0f23bbdcc712fdb774f39c9f7f53ec04abb1
Devin Jaggernauth <neexis@gmail.com>
2022-11-07 22:37:11 -0400

    update context

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,1 +1,16 @@
-export const APP_SECRET = 'secret-safe'
+import * as jwt from 'jsonwebtoken';
+
+export const APP_SECRET = 'secret-safe';
+
+export interface AuthTokenPayload {
+  userId: number;
+}
+
+export function decodeAuthHeader(authHeader: string): AuthTokenPayload {
+  const token = authHeader.replace('Bearer ', '');
+
+  if (!token) {
+    throw new Error('Invalid token');
+  }
+  return jwt.verify(token, APP_SECRET) as AuthTokenPayload;
+}
