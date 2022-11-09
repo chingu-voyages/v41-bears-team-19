015a66b54362d66a401bef932e8bf43033460b34
Devin Jaggernauth <neexis@gmail.com>
2022-11-08 21:21:36 -0400

    setup Apollo Client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,13 +4,31 @@ import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const httplink = createHttpLink({
+  uri: 'http://localhost:5500',
+});
+
+const client = new ApolloClient({
+  link: httplink,
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
